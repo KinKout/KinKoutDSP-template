d9a79d55bddc05b88ac1809268227df064d5d168
KinKout <kinkout@yahoo.com>
2026-08-20 02:47:49 +0200

    chore: add detailed source build log

--- _cmake-config/_scripts/createSourceTemplate.cmake
+++ _cmake-config/_scripts/createSourceTemplate.cmake
@@ -16,7 +16,10 @@ string(TIMESTAMP BUILD_DATE "%d-%b-%Y")
 set(PLUGIN_NAME ${_PROJECT_NAME})
 
 set(TEMPLATE_DIR "${_THIS_DIR}/_cmake-config/template")
-set(_OPTIONAL_SOURCE_LOADED TRUE)
+set(_OPTIONAL_SOURCE_LOADED FALSE)
+
+set(_N_TEMPLATE_LOADED 0)
+set(N_TEMPLATE_NOT_LOADED 0)
 
 set(TEMPLATE_FILES
         "essential:source:PluginProcessor_h"
@@ -25,6 +28,7 @@ set(TEMPLATE_FILES
         "essential:source:PluginEditor_cpp"
 )
 
+list(SORT TEMPLATE_FILES)
 foreach(LIST ${TEMPLATE_FILES})
     string(REPLACE ":" ";" LIST ${LIST})
     list(GET LIST 0 TYPE)
@@ -38,7 +42,8 @@ foreach(LIST ${TEMPLATE_FILES})
  
     if(NOT EXISTS "${SOURCE_FILE}")
         if(TYPE STREQUAL "optional" AND _OPTIONAL_SOURCE_LOADED STREQUAL TRUE)
-            message(STATUS "Skipped (deleted by user): [${TYPE}] ${OUT_NAME}")
+            message("\t-> Excluded:\t[${TYPE}]\t${SUBDIR}/${OUT_NAME}")
+            math(EXPR N_TEMPLATE_NOT_LOADED "${N_TEMPLATE_NOT_LOADED} + 1")
             continue()
         endif()
         configure_file(
@@ -46,11 +51,22 @@ foreach(LIST ${TEMPLATE_FILES})
             "${SOURCE_FILE}"
             @ONLY
         )
-        message(STATUS "Generated: [${TYPE}] ${SUBDIR}/${OUT_NAME}")
+        message("\t-> Generated:\t[${TYPE}]\t${SUBDIR}/${OUT_NAME}")
     else()
-        message(STATUS "Skipped (already exists): [${TYPE}] ${OUT_NAME}")
+        message("\t-> Detected:\t[${TYPE}]\t${SUBDIR}/${OUT_NAME}")
     endif()
+    math(EXPR _N_TEMPLATE_LOADED "${_N_TEMPLATE_LOADED} + 1")
 endforeach()
+
+message("\n\t>>> template source files: ${_N_TEMPLATE_LOADED} files confirmed")
+if(N_TEMPLATE_NOT_LOADED)
+    message("\t                           ${N_TEMPLATE_NOT_LOADED} optional files ignored, deleted by user\n")
+    message(WARNING "If you need to reload the optional source files,\n"
+            "set \"_OPTIONAL_SOURCE_LOADED\" variable to \"FALSE\"\n"
+            "at line 19 in /_cmake-config/_scripts/createSourceTemplate.cmake\" files.\n"
+            "The optional source files will be created in their original path.")
+endif ()
+message("\n")
 
 if(NOT _OPTIONAL_SOURCE_LOADED)
     file(READ "_cmake-config/_scripts/createSourceTemplate.cmake" THIS_FILE)
